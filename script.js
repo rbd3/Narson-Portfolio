ca18594b5064ebc80db61cd75cea3f93ccc7f464
rbd3 <rabedesanaa@gmail.com>
2023-05-16 00:48:40 +0300

    Fix identation error

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const hamburger = document.querySelector('.humburger');
-const menu = document.querySelector('.menu');
 const closeBtn = document.querySelector('.closebtn');
 const workSection = document.querySelector('.work');
 const aboutSection = document.querySelector('.about-me');
@@ -10,28 +9,25 @@ hamburger.addEventListener('click', () => {
 document.getElementById('myNav').style.width = '100%';
 });
 
-  closeBtn.addEventListener('click', () => {
-  document.getElementById('myNav').style.width = '0%';
-  });
+closeBtn.addEventListener('click', () => {
+document.getElementById('myNav').style.width = '0%';
+});
 
-  // Scroll to the work section when the Portfolio menu item is clicked
-  const Portfolio = document.querySelector('#portfolio');
-  Portfolio.addEventListener('click', () => {
-  workSection.scrollIntoView({ behavior: 'smooth' });
-  document.getElementById('myNav').style.width = '0%';
-  });
+const Portfolio = document.querySelector('#portfolio');
+Portfolio.addEventListener('click', () => {
+workSection.scrollIntoView({ behavior: 'smooth' });
+document.getElementById('myNav').style.width = '0%';
+});
 
-  // Scroll to the about section when the About menu item is clicked
-  const about = document.querySelector('#about');
-  about.addEventListener('click', () => {
-  aboutSection.scrollIntoView({ behavior: 'smooth' });
-  document.getElementById('myNav').style.width = '0%';
-  });
+const about = document.querySelector('#about');
+about.addEventListener('click', () => {
+aboutSection.scrollIntoView({ behavior: 'smooth' });
+document.getElementById('myNav').style.width = '0%';
+});
 
-  // Scroll to the contact section when the Contact menu item is clicked
-  const contact = document.querySelector('#contact');
-  contact.addEventListener('click', () => {
-    contactSection.scrollIntoView({ behavior: 'smooth'
-  });
-    document.getElementById('myNav').style.width = '0%';
-  });
+const contact = document.querySelector('#contact');
+contact.addEventListener('click', () => {
+  contactSection.scrollIntoView({ behavior: 'smooth'
+});
+  document.getElementById('myNav').style.width = '0%';
+});
