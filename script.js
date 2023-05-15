56e4f95a9a50419406c47b250653df9a2f0f0d6a
rbd3 <rabedesanaa@gmail.com>
2023-05-16 00:30:40 +0300

    Fix identation error

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-  // Get the necessary DOM elements
   const hamburger = document.querySelector('.humburger');
   const menu = document.querySelector('.menu');
   const closeBtn = document.querySelector('.closebtn');
@@ -6,36 +5,33 @@
   const aboutSection = document.querySelector('.about-me');
   const contactSection = document.querySelector('.contact-form');
 
-  // Show the mobile menu when the hamburger button is clicked
+// Show the mobile menu when the hamburger button is clicked
   hamburger.addEventListener('click', () => {
-    //menu.style.display = "block";
-    document.getElementById("myNav").style.width = '100%';
-
+  document.getElementById('myNav').style.width = '100%';
   });
 
-
   closeBtn.addEventListener('click', () => {
-    document.getElementById("myNav").style.width = "0%";
-  } );
-
+  document.getElementById('myNav').style.width = '0%';
+  });
 
   // Scroll to the work section when the Portfolio menu item is clicked
   const Portfolio = document.querySelector('#portfolio');
   Portfolio.addEventListener('click', () => {
-    workSection.scrollIntoView({ behavior: 'smooth' });
-    document.getElementById("myNav").style.width = "0%";
+  workSection.scrollIntoView({ behavior: 'smooth' });
+  document.getElementById('myNav').style.width = '0%';
   });
 
   // Scroll to the about section when the About menu item is clicked
   const about = document.querySelector('#about');
   about.addEventListener('click', () => {
-    aboutSection.scrollIntoView({ behavior: 'smooth' });
-    document.getElementById("myNav").style.width = "0%";
+  aboutSection.scrollIntoView({ behavior: 'smooth' });
+  document.getElementById('myNav').style.width = '0%';
   });
 
   // Scroll to the contact section when the Contact menu item is clicked
   const contact = document.querySelector('#contact');
   contact.addEventListener('click', () => {
-    contactSection.scrollIntoView({ behavior: 'smooth' });
-    document.getElementById("myNav").style.width = "0%";
+    contactSection.scrollIntoView({ behavior: 'smooth'
+  });
+    document.getElementById('myNav').style.width = '0%';
   });
