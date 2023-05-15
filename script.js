706685d6b129fbd2aefbae959ae37948ff9b28e0
rbd3 <rabedesanaa@gmail.com>
2023-05-16 00:16:04 +0300

    Add mobile menu event

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
   // Get the necessary DOM elements
   const hamburger = document.querySelector('.humburger');
   const menu = document.querySelector('.menu');
-  const closeBtn = document.querySelector('.close');
+  const closeBtn = document.querySelector('.closebtn');
   const workSection = document.querySelector('.work');
   const aboutSection = document.querySelector('.about-me');
   const contactSection = document.querySelector('.contact-form');
 
   // Show the mobile menu when the hamburger button is clicked
   hamburger.addEventListener('click', () => {
-    menu.style.display = "block";
+    //menu.style.display = "block";
+    document.getElementById("myNav").style.width = '100%';
+
   });
+
+
+  closeBtn.addEventListener('click', () => {
+    document.getElementById("myNav").style.width = "0%";
+  } );
 
 
   // Scroll to the work section when the Portfolio menu item is clicked
-  const portfolioMenuItem = document.querySelector('.menu-item:nth-of-type(1)');
-  portfolioMenuItem.addEventListener('click', () => {
+  const Portfolio = document.querySelector('#portfolio');
+  Portfolio.addEventListener('click', () => {
     workSection.scrollIntoView({ behavior: 'smooth' });
+    document.getElementById("myNav").style.width = "0%";
   });
 
   // Scroll to the about section when the About menu item is clicked
-  const aboutMenuItem = document.querySelector('.menu-item:nth-of-type(2)');
-  aboutMenuItem.addEventListener('click', () => {
+  const about = document.querySelector('#about');
+  about.addEventListener('click', () => {
     aboutSection.scrollIntoView({ behavior: 'smooth' });
+    document.getElementById("myNav").style.width = "0%";
   });
 
   // Scroll to the contact section when the Contact menu item is clicked
-  const contactMenuItem = document.querySelector('.menu-item:nth-of-type(3)');
-  contactMenuItem.addEventListener('click', () => {
+  const contact = document.querySelector('#contact');
+  contact.addEventListener('click', () => {
     contactSection.scrollIntoView({ behavior: 'smooth' });
+    document.getElementById("myNav").style.width = "0%";
   });
